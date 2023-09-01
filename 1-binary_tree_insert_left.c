82b27cebf67ece81a948b907b78509c01fdfa6bb
samie54 <126026530+samie54@users.noreply.github.com>
2023-09-01 13:43:45 +1200

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,5 +1,31 @@
-int main(void)
-{
-    binary_tree_t *root;
+#include "binary_trees.h"
 
-    root = binary_tree_node(NULL, 98);
+/**
+ * binary_tree_insert_left - inserts node at left.
+ * @parent: node as left-child.
+ * @value: int value.
+ * Return: pointerr.
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = parent->left;
+	new_node->right = NULL;
+
+	if (parent->left != NULL)
+		parent->left->parent = new_node;
+
+	parent->left = new_node;
+
+	return (new_node);
+}
