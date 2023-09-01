7e6301fd367161605c4b959b2cce7567ab084918
samie54 <126026530+samie54@users.noreply.github.com>
2023-09-01 16:34:15 -0700

    103-binary_tree_rotate_left.c

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,1 +1,28 @@
-;kgpk
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - does left-rotation on bianry.
+ * @tree: tree to rotate.
+ * Return: pointerr.
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *new_node;
+
+	if (tree == NULL || tree->right == NULL)
+		return (tree);
+	new_node = tree->right;
+	if (new_node->left)
+	{
+		new_node->left->parent = tree;
+		tree->right = new_node->left;
+	}
+	else
+	{
+		tree->right = NULL;
+	}
+	new_node->left = tree;
+	new_node->parent = tree->parent;
+	tree->parent = new_node;
+	return (new_node);
+}
