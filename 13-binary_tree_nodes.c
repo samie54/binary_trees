e22eb38416977d4adf03c00ecce6e6d803a3c19a
samie54 <126026530+samie54@users.noreply.github.com>
2023-09-01 14:43:35 +1200

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,13 +1,27 @@
-#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * binary_tree_is_parent - parent node.
+ * @node: node to be count.
+ * Return: int value.
  */
-int main(void)
+int binary_tree_is_parent(const binary_tree_t *node)
 {
-    binary_tree_t *root;
-    size_t nodes;
+	int new_node = 0;
 
+	if (node && (node->left || node->right))
+		new_node = 1;
+	return (new_node);
+}
+
+/**
+ * binary_tree_nodes - counts node with 1 least child in a binary tree.
+ * @tree: tree to be counted.
+ * Return: int value.
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (binary_tree_is_parent(tree))
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+	return (0);
+}
