145cbbd6b1b2ecd71b4b4ea6041b7f0f19885ac5
samie54 <126026530+samie54@users.noreply.github.com>
2023-09-01 14:40:05 +1200

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,13 +1,31 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * binary_tree_is_leaf - checks leaf in binary tree.
+ * @node: node to be checked.
+ * Return: int value.
  */
-int main(void)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    binary_tree_t *root;
-    size_t leaves;
+	int new_node = 0;
+
+	if (node && !(node->left) && !(node->right))
+		new_node = 1;
+	return (new_node);
+}
+
+/**
+ * binary_tree_leaves - counts leaves in binary tree.
+ * @tree: tree to be counted.
+ * Return: int value.
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (binary_tree_is_leaf(tree))
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
