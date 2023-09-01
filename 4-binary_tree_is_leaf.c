1c7ead5d352f91a810c255fbf4a6ebaa89f4bbec
samie54 <126026530+samie54@users.noreply.github.com>
2023-09-01 14:07:58 +1200

    4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,8 +1,15 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * binary_tree_is_leaf - checks if nodee is leaf.
+ * @node: node checked.
+ * Return: int value.
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (node == NULL)
+        return (false);
+    if (node->left == NULL && node->right == NULL)
+        return (true);
+    return (false);
+}
