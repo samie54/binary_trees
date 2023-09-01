8fe062fdf6b14236fc9fe66ecb363bdc8652c077
samie54 <126026530+samie54@users.noreply.github.com>
2023-09-01 16:28:24 -0700

    102-binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
+int binary_tree_is_complete(const binary_tree_t *tree);
+levelorder_queue_t *create_node(binary_tree_t *node);
 void free_queue(levelorder_queue_t *head);
 void push(binary_tree_t *node, levelorder_queue_t *head,
 		levelorder_queue_t **tail);
-levelorder_queue_t *create_node(binary_tree_t *node);
 void pop(levelorder_queue_t **head);
-int binary_tree_is_complete(const binary_tree_t *tree);
 
 /**
  * create_node - new levelorder_queue_t node creation.
