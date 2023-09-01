80b90b824e44e01351fd99c4c7725075ba04de96
samie54 <126026530+samie54@users.noreply.github.com>
2023-09-01 15:21:43 +1200

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,13 +1,49 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * launch_test - Test ancestor function and print informations
- *
- * @n1: First node
- * @n2: Second node
- */
-void launch_test(binary_tree_t *n1, binary_tree_t *n2)
+ * rdepth - depth of node of binary tree.
+ * @tree: tree depth.
+ * Return: node depth.
+ **/
+size_t rdepth(const binary_tree_t *tree)
 {
-    binary_tree_t *ancestor;
+	if (tree == NULL)
+		return (-1);
+
+	return (rdepth(tree->parent) + 1);
+}
+
+/**
+ * binary_tree_depth - calling rdepth.
+ * @tree: tree depth.
+ * Return: depth of node.
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (rdepth(tree));
+}
+
+/**
+ * binary_trees_ancestor - finds lowest common ancestors.
+ * @second: pointer to node.
+ * @first: pointer to node.
+ * Return: pointerr.
+ **/
+binary_tree_t *binary_trees_ancestor(
+	const binary_tree_t *first,
+	const binary_tree_t *second
+)
+{
+	if (second == NULL || first == NULL)
+		return (NULL);
+	if (second == first)
+		return ((binary_tree_t *)first);
+	if (binary_tree_depth(first) > binary_tree_depth(second))
+		return (binary_trees_ancestor(first->parent, second));
+	if (binary_tree_depth(first) < binary_tree_depth(second))
+		return (binary_trees_ancestor(first, second->parent));
+	return (binary_trees_ancestor(first->parent, second->parent));
+}
