2e7edb30601efaf6b51415472fa93c96d42122a8
samie54 <126026530+samie54@users.noreply.github.com>
2023-09-01 14:30:06 +1200

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,16 +1,32 @@
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    binary_tree_t *root;
-    size_t height;
+#include "binary_trees.h"
 
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(root, 128);
-    binary_tree_print(root);
+/**
+ * rheight - measures binary tree's height.
+ * @tree: tree's height.
+ * Return: int value.
+ */
+size_t rheight(const binary_tree_t *tree)
+{
+	size_t a = 0;
+	size_t b = 0;
+
+	if (tree == NULL)
+		return (0);
+	a = rheight(tree->left);
+	b = rheight(tree->right);
+	if (a > b)
+		return (a + 1);
+	return (b + 1);
+}
+
+/**
+ * binary_tree_height - rheight
+ * @tree: tree rheight.
+ * Return: int value.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (rheight(tree) - 1);
+}
