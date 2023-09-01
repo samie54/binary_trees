57bcae137121dabaf27304611668d4906c839560
samie54 <126026530+samie54@users.noreply.github.com>
2023-09-01 15:05:21 +1200

    18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,11 +1,27 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * binary_tree_sibling - sibling a node of binary tree.
+ * @node: node finding.
+ * Return: pointerr.
  */
-int main(void)
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node != node->parent->left)
+		return (node->parent->left);
+	return (node->parent->right);
+}
+
+/**
+ * binary_tree_uncle - finds uncle of a node.
+ * @node: node to be found.
+ * Return: pointerr.
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	return (binary_tree_sibling(node->parent));
+}
