a10c58dc145d02f559b75734b8a5836040aa5a4c
samie54 <126026530+samie54@users.noreply.github.com>
2023-09-01 14:35:04 +1200

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,24 +1,27 @@
+#include "binary_trees.h"
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * rdepth - measures depth of binary tree node.
+ * @tree: tree depth.
+ * Return: 0; if tree is NULL.
  */
-int main(void)
+size_t rdepth(const binary_tree_t *tree)
 {
-    binary_tree_t *root;
-    size_t depth;
+	if (tree == NULL)
+		return (-1);
 
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(root, 128);
-    binary_tree_print(root);
+	return (rdepth(tree->parent) + 1);
+}
 
-    depth = binary_tree_depth(root);
-    printf("Depth of %d: %lu\n", root->n, depth);
-    depth = binary_tree_depth(root->right);
-    printf("Depth of %d: %lu\n", root->right->n, depth);
-    depth = binary_tree_depth(root->left->right);
-    printf("Depth of %d: %lu\n", root->left->right->n, depth);
-    return (0);
+/**
+ * binary_tree_depth - calling rdepth.
+ * @tree: tree depth.
+ * Return: 0; when NULL.
+ **/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (rdepth(tree));
+}
