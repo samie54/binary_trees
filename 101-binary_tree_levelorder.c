beb5aebb4df420c4bda5e16a95a6add583087c75
samie54 <126026530+samie54@users.noreply.github.com>
2023-09-01 15:27:33 +1200

    101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,15 +1,61 @@
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    binary_tree_t *root;
+#include "binary_trees.h"
 
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    root->left->left = binary_tree_node(root->left, 6);
-    root->left->right = binary_tree_node(root->left, 56);
-    root->right->left = binary_tree_node(root->right, 256);
+/**
+ * plevel - prints nodes of binary tree.
+ * @level: level of binary tree node.
+ * @func: pointerr.
+ * @tree: tree of node.
+ *
+ */
+void plevel(const binary_tree_t *tree, int level, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	if (level == 1)
+		func(tree->n);
+	else if (level > 1)
+	{
+		plevel(tree->left, level - 1, func);
+		plevel(tree->right, level - 1, func);
+	}
+}
+
+/**
+ * rheight - heightt of binary tree.
+ * @tree: tree height.
+ * Return: height value.
+ */
+size_t rheight(const binary_tree_t *tree)
+{
+	size_t p = 0;
+	size_t q = 0;
+
+	if (tree == NULL)
+		return (0);
+	p = rheight(tree->left);
+	q = rheight(tree->right);
+	if (p > q)
+		return (p + 1);
+
+	return (q + 1);
+}
+
+/**
+ * binary_tree_levelorder - prints informatiion.
+ * @tree: tree info.
+ * @func: pointerr.
+ *
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	size_t u;
+	size_t v;
+
+	if (tree == NULL || func == NULL)
+		return;
+
+	x = rheight(tree);
+
+	for (v = 1; v <= u; v++)
+		plevel(tree, v, func);
+}
